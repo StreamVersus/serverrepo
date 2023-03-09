aac65ffa0ac09078a9bab2a13386adb1bdf8588d
Vladimir <vov323215@yandex.ru>
2023-03-09 19:00:07 +0300

    start env

---
+++ src/main/java/com/server/test/com/server/test/Com/server/test.java
@@ -0,0 +1,17 @@
+package com.server.test.com.server.test.Com.server;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
